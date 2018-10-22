93144c7b5373a4bf4561bbca76e05f675d852768
jordigar <44298721+jordigar@users.noreply.github.com>
2018-10-22 23:33:06 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# webcam_capture_modified
-From webcam_capture we add some features
+# webcam_capture
+Just webcam capture
+
+## Tips
+https://wiki.archlinux.org/index.php/webcam_setup
+
+http://www.linuxintro.org/wiki/Set_up_a_Webcam_with_Linux
+
+First you need to install "libopencv-dev" using the terminal.
+
+After download the repository, in the terminal you need to compile, create the executable and execute it, below there are the command lines:
+
+cd webcam_capture
+mkdir build
+cd build
+cmake ..
+make
+./webcam_capture
+
+If you add another USB camera for use it add 1 after webcam_capture:
+
+./webcam_capture 1
+
+For working with both cameras at same time you can open two terminals and execute it independent.
---
+++ src/webcam_capture.cpp
@@ -0,0 +1,60 @@
+
+//opencv
+#include "opencv2/opencv.hpp"
+
+//std
+#include <iostream>
+#include <cstdlib>
+
+//main
+int main(int argc, char *argv[])
+{
+    cv::VideoCapture camera; //OpenCV video capture object
+    cv::Mat image; //OpenCV image object
+	int cam_id; //camera id . Associated to device number in /dev/videoX
+	cv::Scalar_<unsigned char> px_value; //pixel value (4-element vector)
+	int user_key; //user pressed key to quit
+
+	//check user args
+	switch(argc)
+	{
+		case 1: //no argument provided, so try /dev/video0
+			cam_id = 0;
+			break;
+		case 2: //an argument is provided. Get it and set cam_id
+			cam_id = atoi(argv[1]);
+			break;
+		default:
+			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl;
+			std::cout << "EXIT program." << std::endl;
+			break;
+	}
+
+	//advertising to the user
+	std::cout << "Opening video device " << cam_id << std::endl;
+
+    //open the video stream and make sure it's opened
+    if( !camera.open(cam_id) )
+	{
+        std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
+        return -1;
+    }
+
+    //capture loop. Out of user press a key
+    while(1)
+	{
+		//Read image and check it. Blocking call up to a new image arrives from camera.
+        if(!camera.read(image))
+		{
+            std::cout << "No frame" << std::endl;
+            cv::waitKey();
+        }
+
+        //show image in a window
+        cv::imshow("Output Window", image);
+	cv::imshow("Output Window 2", image);
+
+		//Waits 30 millisecond to check if 'q' key has been pressed. If so, breaks the loop. Otherwise continues.
+    	if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break; 
+    }
+}
